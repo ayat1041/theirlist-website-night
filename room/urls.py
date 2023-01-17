efde8a80daeba9b2e53337681b27f2b8f5eff16d
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-01-17 11:55:49 +0600

    url up

--- room/urls.py
+++ room/urls.py
@@ -4,6 +4,7 @@ from .views import room_form
 
 urlpatterns = [
     path('',views.rooms, name='rooms'),
-    path('<slug:slug>/',views.room, name='room'),
     path('create_room/',views.room_form,name="create_room"),
+    path('<slug:slug>/',views.room, name='room'),
+    
 ]
